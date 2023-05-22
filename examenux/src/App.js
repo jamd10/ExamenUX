f817b349921cd340907c741497dc37ae6d86d5f6
jamd10 <jameraz28@gmail.com>
2023-05-21 18:47:57 -0600

    Carrusel

--- examenux/src/App.js
+++ examenux/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import React from 'react';
+import Carrusel from './Carrusel';
+
 function App() {
   return (
     <>
       <Navbar />
+      <Carrusel />
     </>
   );
 }
